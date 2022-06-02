87f3e3e24065e2699ca0255ecfb3c3c79d874d87
1iutao <100211146+1iutao@users.noreply.github.com>
2022-06-02 15:18:44 +0800

    Delete SpringDataJpa directory

--- SpringDataJpa/src/main/java/springboot/demo/springdatajpa/repository/UserRepository2.java
+++
@@ -1,9 +0,0 @@
-package springboot.demo.springdatajpa.repository;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-import springboot.demo.springdatajpa.entity.UserDo;
-
-public interface UserRepository2 extends PagingAndSortingRepository<UserDo,Integer> {
-
-    //继承自自动分页仓库
-}
